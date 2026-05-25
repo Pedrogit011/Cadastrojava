b2df2cde92b9c6ea23002292e46fee0da00f316c
Pedrogit011 <pedro.silva01102625@sesisenaipr.org.br>
2026-05-25 11:14:06 -0300

    Add files via upload

---
+++ Condicao.java
@@ -0,0 +1,10 @@
+public class Condicao {
+    public static void main(String[] args) {
+        int idade = 67;
+        if(idade >= 18) {
+            System.out.println("Maior de idade");
+        } else {
+            System.out.println("Menor de idade");
+        }
+    }
+}
---
+++ Dados.java
@@ -0,0 +1,11 @@
+public class Dados {
+    public static void main(String[] args) {
+        String nome = "Pedro";
+        int idade = 15;
+        double altura = 1.87;
+       
+        System.out.println(nome);
+        System.out.println(idade);
+        System.out.println(altura);
+    }
+}
